2865ef501e3c971ce5e68d40bcff72eba824ace5
Louis-Marie Matthews <internet657-github@yahoo.fr>
2017-02-28 00:05:22 +0000

    Merge everything in one page with fullPage.js

--- include/html.php
+++ include/html.php
@@ -1,8 +1,5 @@
 <?php
 
-// TODO: to refactor
-$root_uri = 'http://shift-two.alwaysdata.net/portfolio/';
-
 function display_head( $title ) {
     ?>
     <meta charset="utf-8" />
@@ -17,36 +14,20 @@ function display_head( $title ) {
     <link rel="stylesheet" href="owlcarousel/assets/owl.carousel.min.css">
     <link rel="stylesheet" href="owlcarousel/assets/owl.theme.default.min.css">
 
+    <link rel="stylesheet" href="style.css" />
+
+    <link rel="stylesheet" type="text/css" href="fullpagejs/jquery.fullpage.min.css" />
+
+
+
     <?php
 }
 
 
 
-function display_main_navbar( $current_page ) {
-  global $root_uri;
-    switch ( $current_page ) {
-        case 'home':
-        $class_home = 'active';
-        break;
-
-        case 'level_design_and_blender':
-        $class_level_design_and_blender = 'active';
-        break;
-
-        case 'contact':
-        $class_contact = 'active';
-        break;
-
-        case 'programming':
-        $class_programming = 'active';
-        break;
-
-        case 'community':
-        $class_community = 'active';
-        break; 
-    }
+function display_main_navbar() {
     ?>
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -59,11 +40,11 @@ function display_main_navbar( $current_page ) {
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="<?php echo( $class_home ) ?>"><a href="<?php echo( $root_uri ) ?>">Home</a></li>
-            <li class="<?php echo( $class_level_design_and_blender ) ?>"><a href="<?php echo( $root_uri ) ?>level-design-and-blender/">Level Design & Blender</a></li>
-            <li class="<?php echo( $class_programming ) ?>"><a href="<?php echo( $root_uri ) ?>programming/">Programming</a></li>
-            <li class="<?php echo( $class_community ) ?>"><a href="<?php echo( $root_uri ) ?>community/">Community</a></li>
-            <li class="<?php echo( $class_contact ) ?>"><a href="<?php echo( $root_uri ) ?>contact/">Contact</a></li>
+            <li class="active"><a href="#top">Home</a></li>
+            <li><a href="#community">Community</a></li>
+            <li><a href="#level-design-and-blender">Level Design & Blender</a></li>
+            <li><a href="#programming">Programming</a></li>
+            <li><a href="#contact">Contact</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -100,12 +81,16 @@ function display_footer() {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="owlcarousel/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="fullpagejs/jquery.fullpage.min.js"></script>
     <script>
         $(document).ready(function(){
             $(".owl-carousel").owlCarousel({
               items: 1
             });
-            });
+        });
+        $(document).ready(function() {
+            $('#fullpage').fullpage();
+        });
     </script>
     <?php
 }
--- index.php
+++ index.php
@@ -6,8 +6,69 @@
     </head>
     <body>
         <?php display_main_navbar( 'home' ) ?>
-        <div class="container">
-            <?php display_carousel() ?>
+        <div id="fullpage">
+            <div class="section">
+                <div id="top" class="container">
+                    <?php display_carousel() ?>
+                </div>
+            </div>
+            <div class="section">
+                <div class="container">
+                    <h1 id="community">Community</h1>
+                    <p>As I was learning, and later practising level design, I noticed level designers and resources for Half-Life 1, such as tutorials, were scattered 
+                    accross a variety of small websites, threateting the community and the creations of everywhone. Beside, it was sometimes hard for beginners to know
+                    where to start and what community to join.</p>
+                    <p>This is why, with a few other members of the website OpenClassrooms, we decided to create a website aimed at gathering all the level designers for 
+                    any Goldsource games (including Half-Life 1), and creating and sharing resources such as video tutorials.</p>
+                    <p>The website is unavailable at the moment, but a new version of it is in preparation. In the meantime, we've still got our Youtube channel onto which 
+                    I upload video tutorials I make, our Steam group composed of level designers I found and contacted accross the Internet, and our Facebook, Twitter and
+                    Google+ pages, where I regularly share new contents related to the Goldsource world.</p>
+                </div>
+            </div>
+            <div class="section">
+                <div class="container">
+                    <h1 id="level-design-and-blender">Level Design & Blender</h1>
+                    <p>My first experience with 3D modelisation goes back in 2010. This is when I discovered how 3D objects and environments in video games where made thanks to a tutorial 
+                    of OpenClassrooms.</p>
+                    <p>I became passionate about creating my own 3D worlds and this culminated in the release of my very own video game, Shift-Two, in April 2011.
+                    Shift-Two is a mod, which means it is a video game reusing a variable amount of assets from a another video game, in this case Half-Life 1. The mod brings new levels,
+                    a different storyset and new textures to the original game. I was willing to create a website for my mod, and this was the perfect occasion to get a first glimpse into
+                    web development. I learnt HTML and CSS and used these technologies to create my very first website.</p>
+                    <p>I continued to practice mapping over the following years (and tried to gather and strengthen the level designers community). I jumped into the next step which was
+                    learn Blender in 2014 in order to bring more details to my levels.
+                    This powerful software, very different from Valve Hammer Editor (the software used to create levels for Half-Life 1 games), was challenging to learn.
+                    Also challenging was the integration of my Blender models in levels designed for a game engine about 20 years old. This required using softwares and plugins to convert
+                    my files from the .blend format to the .mdl format, used by my mod.</p>
+                    <p>The idea of using my skills to recreate my house was floating in my mind for quite a long time. The proposal of a new online community mod was for me a chance to 
+                    bring that idea to reality. This involved a lot of work over the summer. I had to take the dimensions of an important amount of objects in my house in order to recreate
+                    them in my map. In addition to being decoration, they also provided me with a visual clue of the dimension of the structure of my level, and I was able to use them
+                    as some sort of landmarks in order to get the dimensions of my walls, floors and ceilings correct.</p>
+                </div>
+            </div>
+            <div class="section">
+                <div class="container">
+                    <h1 id="programming">Programming</h1>
+                    <p>The first programming language I learnt was PHP, reading a tutorial of OpenClassrooms.
+                    Its simple syntax made it a perfect introduction to programming languages for me.
+                    Although I did not learn it for any specific purposes, it would later prove extremely useful as it allowed me to be prepared for some university assignments, 
+                    become the webmaster of Dundee University Review of the Arts and also understand the logic of a programming languages, making learning other languages much
+                    more easier.</p>
+                    <p>I learnt Java, ASP.NET, Arduino and Processing as part of my university course. In second year, we were tasked to make a video game using the technology
+                    of our choice. I decided to learn C++ and use Allegro to draw 2D graphics as it was the perfect opportunity for me to learn and gain experience in this 
+                    ubiquitous language, which would afterwards allow me to learn OpenGL and contribute to community projects related to Half-Life 1 modding.</p>
+                </div>
+            </div>
+            
+            <div class="section">
+                <div class="container">
+                    <h1 id="contact">Contact</h1>
+                    <ul>
+                        <li><a href="https://github.com/Louis-MarieMatthews">GitHub</li>
+                        <li><a href="http://stackoverflow.com/users/7089212/louis-marie-matthews">Stack Overflow</li>
+                        <li><a href="https://www.linkedin.com/in/louis-marie-matthews/">LinkedIn</li>
+                    </ul>
+                </div>
+            </div>
         </div>
         <?php display_footer() ?>
     </body>
