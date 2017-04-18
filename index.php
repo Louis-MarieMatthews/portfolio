3ff4aeeb7d9d272598a21aa1fa906cf2f774e7fd
Louis-Marie Matthews <internet657-github@yahoo.fr>
2017-04-19 00:45:43 +0200

    Update content
    This commit merges the section about me into one and adds sections corresponding to a specific (or more) project (s).

--- index.php
+++ index.php
@@ -10,33 +10,43 @@
             <div class="section home-section">
                 <div class="pf-section-content">
                     <p>Second-year student in Applied Computing: Human Computer Interaction at the University of Dundee, Scotland.</p>
-                    <p>I enthousiastically keep learning new technologies and perfecting my skills, enjoy logical thinking and excel at problem solving.</p>
-                </div>
-            </div>
-            <div class="section programming-section">
-                <div class="pf-section-content">
-                    <p>I taught myself HTML5, CSS3, Javascript, PHP, Python and C++ and keep learning either online, or by reading books and attending conferences such as the Dundee PHP Meetup.</p>
-                </div>
-            </div>
-            <div class="section thoroughness-section">
-                <div class="pf-section-content">
-                    <p>Although the final deliverable is my absolute priority, I take great care to deliver high-quality code by sytematically applying time-tested programming principles (DRY, SOLID). and design patterns. In regard to web development, I follow the W3C Accessibility Guidelines and ensure my websites are responsive.</p>
+                    <p>I have experience in Blender, HTML5, CSS3, PHP, C++, Javascript and C# / ASP.NET and teamwork.</p>
+                    <p>I strive to deliver quality and excel at logical thinking. I keep learning new technologies and perfecting my skills. I follow established programming principles (DRY, SOLID) and design patterns.</p>
+                    <ul>
+                        <li><a href="mailto:louismarie.matthews@outlook.fr">Email</a></li>
+                        <li><a href="https://github.com/Louis-MarieMatthews">GitHub</a></li>
+                        <li><a href="https://www.linkedin.com/in/louis-marie-matthews/">LinkedIn</li>
+                        <li><a href="http://stackexchange.com/users/9540988/louis-marie-matthews?tab=top">Stack Exchange</a></li>
+                    </ul>
                 </div>
             </div>
             <div class="section shift-two-section">
                 <div class="pf-section-content">
                     <p>Shift-Two is a mod that I made on my free time when I was in secondary school. The experience I gained from the 2 years I spent working on it would later prove extremely useful in managing personal as well as team projects.</p>
-                    <p>I learnt HTML, CSS and the basics of 3D modelling. The techonoly used being old and closed-source, I also gained a lot of experience in learning through trial-and-errors.</p>
+                    <p>I learnt HTML, CSS and the basics of 3D modelling. The technology used being old and closed-source, I also gained a lot of experience in learning through trial-and-errors.</p>
                 </div>
             </div>
             <div class="section dura-section">
                 <div class="pf-section-content">
-                    <p>Entirely revamping a Wordpress theme to meet the client's demands made for a challenging and productive experience. Wordpress, Javascript, Bootstrap and Git were technologies I had to quickly become familiar with to deliver the theme on time.</p>
+                    <p>Entirely revamping a Wordpress theme to meet the client's demands made for a challenging and productive experience, even more when the other student working with me withdrew from the project.</p>
+                    <p>Wordpress, Javascript, Bootstrap and Git were technologies I had to quickly become familiar with to deliver the theme on time.</p>
                 </div>
             </div>
             <div class="section java-and-php-section">
                 <div class="pf-section-content">
-                    <p>I've used PHP and Java for several university assignments, which can be found on Github.</p>
+                    <p>I've used PHP and Java for several university assignments, which can be found on Github. These are: </p>
+                    <ul>
+                        <li>A <a href="https://github.com/Louis-MarieMatthews/EventQueueExample">Java console program</a> which makes use of threads to explain the event queue design pattern.</li>
+                        <li>A <a href="https://github.com/Louis-MarieMatthews/privateinstagrim">Java Website</a> which is a refactored version of <a href="https://github.com/acobley/instagrim">Instragrim</a>, with multiple bugs fixed and new features.</li>
+                        <li>A <a href="https://github.com/Louis-MarieMatthews/ac32006_assignment1">PHP Website</a> made as a team.</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="section hub-section">
+                <div class="pf-section-content">
+                    <p>I joined an online community project which consisted in putting together the levels made by each participant.</p>
+                    <p>I chose to recreate a real-world location. This proved extremely difficult as the amount of details was enourmous. Besides, staying consistent with the real-world dimensions was equally challenging without specialised material.</p>
+                    <p>I spent a big part of my summer working on this project and recreating with the correct dimensions each object from the real world (windows, tables…).</p>
                 </div>
             </div>
         </div>
