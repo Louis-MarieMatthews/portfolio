7ea6f30fb74ae7d6614e52b2e04109bd521ece32
Louis-Marie Matthews <internet657-github@yahoo.fr>
2017-04-18 23:22:03 +0200

    Remove obsolete function

--- include/html.php
+++ include/html.php
@@ -23,21 +23,6 @@ function display_head( $title ) {
 
 
 
-function display_fb_js_sdk() {
-  ?>
-  <div id="fb-root"></div>
-  <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=775683239250073";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
-  <?php
-}
-
-
-
 function display_main_navbar() {
     ?>
     <nav class="navbar navbar-inverse navbar-fixed-top">
