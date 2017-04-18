155189be7525f0b35e94e365d6a5bafff822ef06
Louis-Marie Matthews <internet657-github@yahoo.fr>
2017-04-18 23:52:21 +0200

    Add some sections

--- index.php
+++ index.php
@@ -23,9 +23,20 @@
                     <p>Although the final deliverable is my absolute priority, I take great care to deliver high-quality code by sytematically applying time-tested programming principles (DRY, SOLID). and design patterns. In regard to web development, I follow the W3C Accessibility Guidelines and ensure my websites are responsive.</p>
                 </div>
             </div>
-            <div class="section level-design-and-blender-section">
+            <div class="section shift-two-section">
                 <div class="container">
-                    <p>I am passionate about creating virtual worlds, this is the reason why I taught myself Blender and created several maps and a mod (a 3D video game using the engine and some of the resources of another video game).</p>
+                    <p>Shift-Two is a mod that I made on my free time when I was in secondary school. The experience I gained from the 2 years I spent working on it would later prove extremely useful in managing personal as well as team projects.</p>
+                    <p>I learnt HTML, CSS and the basics of 3D modelling. The techonoly used being old and closed-source, I also gained a lot of experience in learning through trial-and-errors.</p>
+                </div>
+            </div>
+            <div class="section dura-section">
+                <div class="container">
+                    <p>Entirely revamping a Wordpress theme to meet the client's demands made for a challenging and productive experience. Wordpress, Javascript, Bootstrap and Git were technologies I had to quickly become familiar with to deliver the theme on time.</p>
+                </div>
+            </div>
+            <div class="section java-and-php-section">
+                <div class="container">
+                    <p>I've used PHP and Java for several university assignments, which can be found on Github.</p>
                 </div>
             </div>
         </div>
