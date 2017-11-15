628c3db01099250ccff5c00b2601db65f6ff7bf3
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-15 19:51:24 +0000

    Remove body content and change indentation level to 2

--- index.php
+++ index.php
@@ -1,70 +1,9 @@
 <?php require_once( 'include/html.php' ) ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php display_head( 'Louis-Marie Matthews\'s Portfolio' ) ?>
-    </head>
-    <body>
-        <?php display_main_navbar() ?>
-        <main id="fullpage">
-            <section class="section me-section" aria-labelledby="pf-me-section-heading">
-                <div class="pf-section-content">
-                    <h1 id="pf-me-section-heading" class="section-heading">Me</h1>
-                    <p>Second-year student in Applied Computing: Human Computer Interaction at the University of Dundee, Scotland.</p>
-                    <p>I have experience in Blender, HTML5, CSS3, PHP, C++, Javascript and C# / ASP.NET and teamwork.</p>
-                    <p>I strive to deliver quality and excel at logical thinking. I keep learning new technologies and perfecting my skills. I follow established programming principles (DRY, SOLID) and design patterns.</p>
-                    <ul class="pf-section-nav-ul">
-                        <li><a class="pf-section-nav-ul-link" href="mailto:matthewslouismarie@gmail.com">Email</a></li>
-                        <li><a class="pf-section-nav-ul-link" href="https://github.com/matthewslouismarie">GitHub</a></li>
-                        <li><a class="pf-section-nav-ul-link" href="https://www.linkedin.com/in/matthewslouismarie/">LinkedIn</a></li>
-                        <li><a class="pf-section-nav-ul-link" href="http://stackexchange.com/users/9540988/matthewslouismarie">Stack Exchange</a></li>
-                    </ul>
-                </div>
-            </section>
-            <section class="section shift-two-section" aria-labelledby="pf-shift-two-section-heading">
-                <div class="pf-section-content">
-                    <h1 id="pf-shift-two-section-heading" class="section-heading">Shift-Two</h1>
-                    <p>Shift-Two is a mod that I made on my free time when I was in secondary school. The experience I gained from the 2 years I spent working on it would later prove extremely useful in managing personal as well as team projects.</p>
-                    <p>I learnt HTML, CSS and the basics of 3D modelling. The technology used being old and closed-source, I also gained a lot of experience in learning through trial-and-errors.</p>
-                <ul class="pf-section-nav-ul">
-                        <li><a class="pf-section-nav-ul-link" href="http://shift-two.alwaysdata.net/">Visit Shift-Two's website</a></li>
-                    </ul>
-                </div>
-            </section>
-            <section class="section dura-section" aria-labelledby="pf-dura-section-heading">
-                <div class="pf-section-content">
-                    <h1 id="pf-dura-section-heading" class="section-heading">Dundee University Review of the Arts</h1>
-                    <p>Entirely revamping a Wordpress theme to meet the client's demands made for a challenging and productive experience, even more when the only person working with me withdrew from the project.</p>
-                    <p>Wordpress, Javascript, Bootstrap and Git were technologies I had to quickly become familiar with to deliver the theme on time.</p>
-                    <ul class="pf-section-nav-ul">
-                        <li><a class="pf-section-nav-ul-link" href="https://dura-dundee.org.uk/">Visit DURA</a></li>
-                    </ul>
-                </div>
-            </section>
-            <section class="section java-and-php-section" aria-labelledby="pf-java-and-php-section-heading">
-                <div class="pf-section-content">
-                    <h1 id="pf-java-and-php-section-heading" class="section-heading">Java & PHP</h1>
-                    <p>I've used PHP and Java for several university assignments and personal projects, which can be found on Github. These are: </p>
-                    <ul>
-                        <li>A <a href="https://github.com/matthewslouismarie/EventQueueExample">Java console program</a> which makes use of threads to explain the event queue design pattern.</li>
-                        <li>A <a href="https://github.com/matthewslouismarie/privateinstagrim">Java Website</a> which is a refactored version of <a href="https://github.com/acobley/instagrim">Instagrim</a>, with multiple bugs fixed and new features.</li>
-                        <li>A <a href="https://github.com/matthewslouismarie/ac32006_assignment1">PHP Website</a> made as a team.</li>
-                        <li>This very portfolio which source code can be found <a href="https://github.com/matthewslouismarie/portfolio">there</a></li>
-                    </ul>
-                </div>
-            </section>
-            <section class="section twhl-hub-section" aria-labelledby="pf-twhl-hub-section-heading">
-                <div class="pf-section-content">
-                    <h1 id="pf-twhl-hub-section-heading" class="section-heading">TWHL Hub</h1>
-                    <p>I joined an online community project which consisted in putting together the levels made by each participant.</p>
-                    <p>I chose to recreate a real-world location. This proved extremely difficult as the amount of details was enourmous. Besides, staying consistent with the real-world dimensions was equally challenging without specialised material.</p>
-                    <p>I spent a big part of my summer working on this project and recreating with the correct dimensions each object from the real world (windows, tables…).</p>
-                    <ul class="pf-section-nav-ul">
-                        <li><a class="pf-section-nav-ul-link" href="http://twhl.info/forums.php?thread=18885">Visit TWHL Hub thread</a></li>
-                    </ul>
-                </div>
-            </section>
-        </main>
-        <?php display_footer() ?>
-    </body>
+  <head>
+    <?php display_head( 'Louis-Marie Matthews\'s Portfolio' ) ?>
+  </head>
+  <body>
+  </body>
 </html>
