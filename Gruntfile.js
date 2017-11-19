1a3e36609f7f0d2d4925f61b52bb42ca846f07bf
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-19 16:47:21 +0000

    Add grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,18 @@ module.exports = function(grunt) {
             'style.min.css':['sass/style.scss'],
           }
         }
+      },
+      watch: {
+        sass: {
+          files: ['sass/*.scss'],
+          tasks: ['sass']
+        }
       }
     });
   
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');    
   
     grunt.registerTask('default', ['sass']);
   };
