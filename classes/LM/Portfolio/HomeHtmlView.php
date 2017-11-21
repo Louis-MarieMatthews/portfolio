4854d71b5ab53b8bdd8fbe4472c854b08c32457d
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-21 16:24:54 +0000

    Revert back to external stylesheet file

--- classes/LM/Portfolio/HomeHtmlView.php
+++ classes/LM/Portfolio/HomeHtmlView.php
@@ -28,7 +28,7 @@ class HomeHtmlView implements IView
     <title>Home</title>
     <link rel="icon" href="img/favicon.ico">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed">
-    <style><?= include 'style.min.css' ?></style>
+    <link rel="stylesheet" href="style.min.css">
   </head>
   <body class="page" itemscope itemtype="http://schema.org/CollectionPage">  
     <script>
