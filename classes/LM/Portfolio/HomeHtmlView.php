5e7b466f8acd880edf25c4cd57e4033f4abba04d
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-21 15:57:58 +0000

    Add facebook analytics

--- classes/LM/Portfolio/HomeHtmlView.php
+++ classes/LM/Portfolio/HomeHtmlView.php
@@ -33,7 +33,28 @@ class HomeHtmlView implements IView
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed">
     <link rel="stylesheet" href="style.min.css">
   </head>
-  <body class="page" itemscope itemtype="http://schema.org/CollectionPage">
+  <body class="page" itemscope itemtype="http://schema.org/CollectionPage">  
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '2013357305611860',
+          cookie     : true,
+          xfbml      : true,
+          version    : 'v2.11'
+        });
+          
+        FB.AppEvents.logPageView();   
+          
+      };
+
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
     <header itemscope itemprop="author" itemtype="http://schema.org/Person">
       <h1 class="page-title">Who am I?</h1>
       <div class="content-container">
