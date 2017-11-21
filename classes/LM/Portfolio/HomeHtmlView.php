9947cc0107b0fd79874e12d83f2b6dfc64ca2263
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-21 18:01:21 +0000

    Remove inline facebook analytics script from AMP view

--- classes/LM/Portfolio/HomeHtmlView.php
+++ classes/LM/Portfolio/HomeHtmlView.php
@@ -17,6 +17,27 @@ class HomeHtmlView extends TemplateHtmlView
     <?php $this->displayHeadContent() ?>
   </head>
   <body <?php $this->displayBodyAttrs() ?>>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '2013357305611860',
+          cookie     : true,
+          xfbml      : true,
+          version    : 'v2.11'
+        });
+          
+        FB.AppEvents.logPageView();   
+          
+      };
+
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
     <?php $this->displayBodyContent() ?>
   </body>
 </html>
--- classes/LM/Portfolio/TemplateHtmlView.php
+++ classes/LM/Portfolio/TemplateHtmlView.php
@@ -30,27 +30,6 @@ class TemplateHtmlView implements IView
     public function displayBodyContent()
     {
 ?>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '2013357305611860',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.11'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
-
-  (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script>
 <header itemscope itemprop="author" itemtype="http://schema.org/Person">
   <h1 class="page-title">Who am I?</h1>
   <div class="content-container">
