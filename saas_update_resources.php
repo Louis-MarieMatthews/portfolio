01dfbc7fc70dc3424739108e9cc1e0a0ea24b3a2
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-27 00:15:06 +0000

    Make saas_update_resources slightly more robust

--- saas_update_resources.php
+++ saas_update_resources.php
@@ -27,23 +27,24 @@ function version_resources(string $stylesheet_filename): void
 
     $preg_match_return_code = preg_match_all('/img\/[a-z]+\.[a-z]+/', $style, $match_categories);
 
-    $image_matches = $match_categories[0];
-
     if (false === $preg_match_return_code) {
         throw new \Exception();
     }
 
     $updated_style = $style;
-    foreach ($image_matches as $current_image) {
-        $img_parts = explode('.', $current_image);
-        $img_parts_length = count($img_parts);
+    var_dump($match_categories);
+    foreach ($match_categories as $image_matches) {
+        foreach ($image_matches as $current_image) {
+            $img_parts = explode('.', $current_image);
+            $img_parts_length = count($img_parts);
 
-        $filename = $img_parts[0];
-        for ($i = 1; $i <  - 1; $i++) {
-            $filename .= $img_parts[$i];
+            $filename = $img_parts[0];
+            for ($i = 1; $i <  - 1; $i++) {
+                $filename .= $img_parts[$i];
+            }
+            
+            $updated_style = str_replace($current_image, getVersionedUri($filename, '.'.$img_parts[$img_parts_length - 1]), $updated_style);
         }
-        
-        $updated_style = str_replace($current_image, getVersionedUri($filename, '.'.$img_parts[$img_parts_length - 1]), $updated_style);
     }
 
     $fpc_return_code = file_put_contents($stylesheet_filename, $updated_style);
